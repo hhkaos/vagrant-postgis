6cec77e0699815c2ffc47939485a4bed0be2764c
Raul Jimenez Ortega <raul.jimenez@esri.es>
2020-08-13 08:33:21 +0200

    Shell script to export table to shp

---
+++ utils/exportLayer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Exporting table: $1"
+pgsql2shp -u vagrant -h 127.0.0.1 -P vagrant -p 5432 -f "$1.shp" postgisDB "SELECT * F$
+process_id=$!
+
+wait $process_id
+echo "Compressing $1"
+zip "$1.zip" $1.*
+rm $1.dbf $1.shp $1.shx $1.prj $1.cpg
